88aa450581943454b948ba42ac88af302004257f
Poololol <81496600+Poololol@users.noreply.github.com>
2025-07-26 15:26:18 -0500

    clean up files and move the search into a cog

--- bot.py
+++ bot.py
@@ -3,28 +3,25 @@ import json
 import time
 import sendWork
 import discord
-from discord import TextChannel, VoiceChannel, StageChannel, DMChannel, PartialMessageable, GroupChannel, Thread
-from typing import Union
-import threading
-
-PartialMessageableChannel = Union[TextChannel, VoiceChannel, StageChannel, Thread, DMChannel, PartialMessageable]
-MessageableChannel = Union[PartialMessageableChannel, GroupChannel]
+import search
 
 class Bot(discord.Client):
-    def setInfo(self, searchParams: dict[str, str | bool | int], updateTime: float, channels: list[MessageableChannel]) -> None:
+    def setInfo(self, searchParams: dict[str, str | bool | int], updateTime: float, channelIDs: list[int]) -> None:
         '''
         Args:
             searchParams (dict[str, str]): 
             updateTime (float): The time between two different requests in hours
+            channelIDs (list[int]): The list of channel IDs to send messages to
         '''
         self.searchParams = searchParams
         self.updateTime = updateTime
-        self.channels = channels
+        self.channelIDs = channelIDs
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
-        await self.search()
+        #threading.Thread(target=self.search).start()
+        search.searchCog()
     
     async def on_message(self, message: discord.Message):
         if message.author == self.user:
@@ -33,8 +30,7 @@ class Bot(discord.Client):
         if message.content.startswith('$setChannel'):
             with open('data.json') as file:
                 data = json.JSONDecoder().decode(file.read())
-            #data['channels'].append(message.channel.)
-            self.channels.append(message.channel)
+            data['channelIDs'].append(message.channel.id)
             with open('data.json', 'w') as file:
                 file.write(json.JSONEncoder().encode(data))
 
@@ -44,41 +40,14 @@ class Bot(discord.Client):
             with open('data.json') as file:
                 data = json.JSONDecoder().decode(file.read())
             try:
-                #data['channels'].remove(message.channel)
-                self.channels.remove(message.channel)
+                data['channelIDs'].remove(message.channel.id)
                 await message.channel.send(f'Removed channel {message.channel.name} from update list') #type: ignore
             except ValueError:
                 await message.channel.send(f'Failed to remove channel {message.channel.name} from update list') #type: ignore
             with open('data.json', 'w') as file:
                 file.write(json.JSONEncoder().encode(data))
-
-    async def search(self) -> None:
-        search = AO3.Search(**self.searchParams) #type: ignore
-        search.update()
-        print(search.total_results)
-
-        decoder = json.decoder.JSONDecoder()
-        while True:
-            t1 = time.time()
-            dataFile = decoder.decode(open('data.json').read())
-            totalWorks = dataFile['total']
-            startingWorks = dataFile['total']
-            while totalWorks < search.total_results:
-                for result in search.results:
-                    if result.id not in dataFile['ids']:
-                        self.sendWork(result.id)
-                        dataFile['ids'].append(result.id)
-                        totalWorks += 1
-                print(totalWorks)
-                search.page += 1
-                search.update()
-            dataFile['total'] = totalWorks
-            with open('data.json', 'w') as file:
-                jsonData = json.JSONEncoder().encode(dataFile)
-                file.write(jsonData)
-            print(f'Updated Data File in {round(time.time() - t1, 1)} Seconds\nIncreased works from {startingWorks} to {totalWorks}')
-            time.sleep(self.updateTime * 60 * 60)
     
     def sendWork(self, workID):
-        for channel in self.channels:
-            message = sendWork.sendWork(workID, channel)
+        for channelID in self.channelIDs:
+            channel = self.get_channel(channelID)
+            message = await channel.send(f'https://archiveofourown.org/works/{workID}')
